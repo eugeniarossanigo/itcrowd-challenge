eed39f1d4158d4b0dbbde908cc27f909599bf2c5
eugeniarossanigo <eugeniarossanigo@gmail.com>
2023-11-28 12:22:21 -0300

    add router for pages and general styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './layouts/Layout'
+import Home from './pages/Home'
+import ItemDetail from './pages/ItemDetail'
+// import Contact from './pages/Contact'
 
 function App() {
+  const router = createBrowserRouter([
+    { path: '/', element: <Layout />,
+      children: [
+        { path: '/', element: <Home /> },
+        { path: '/products/:id', element: <ItemDetail /> },
+      ]
+    }
+  ])
 
   return (
     <>
+      <RouterProvider router={router} />
     </>
   )
 }
