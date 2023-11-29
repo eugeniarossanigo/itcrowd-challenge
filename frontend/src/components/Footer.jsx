4a37bc3f40ff31703b130d214041a04ed84de7d1
eugeniarossanigo <eugeniarossanigo@gmail.com>
2023-11-29 15:13:20 -0300

    update header and footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer id='main-footer' className='flex justify-center'>
-      <div>Footer</div>
+    <footer id='main-footer' className='w-full bg-light-green flex justify-center items-center'>
+      <p className='text-xs'>Eugenia Rossanigo - All rights reserved © 2023</p>
     </footer>
   )
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,18 +1,31 @@
 import { Link as LinkRouter } from 'react-router-dom'
+import { FaSearch } from 'react-icons/fa';
+import { RiUserAddLine, RiUserFollowLine, RiUserLine } from 'react-icons/ri';
 
 export default function Header() {
   return (
-    <header id='main-header' className='w-2/3 flex justify-between'>
-      <img src='/logo.png' className='h-[12vh]' alt='logo' />
-      <nav>
-        <LinkRouter to='/'>Home</LinkRouter>
-        <LinkRouter to='/'>Test</LinkRouter>
-      </nav>
-      <section className='flex'>
-        <label>🔍
-          <input type='search' className='border border-1'/>
-        </label>
-        <LinkRouter to='/' ><img src='/add-user.png' alt='user' className='w-5'/></LinkRouter>
+    <header id='main-header' className='w-full flex flex-col items-center shadow-[0_20px_40px_-20px_rgba(166,171,177,1)]'>
+      <section className='w-full h-[4vh] tracking-wider bg-moss-green text-center text-[.6rem] text-white font-semibold pt-3'>
+        <p>New incomes soon!</p>
+      </section>
+      <section className='w-3/4 h-full lg:h-[16vh] flex flex-col lg:flex-row justify-between items-center py-4 gap-6'>
+        <div className='h-[6vh]'>
+          <img src='/logo.png' className='h-full rotate-[-12deg] py-1' alt='logo' />
+        </div>
+        <nav className='uppercase text-xs tracking-[2px] flex items-center gap-6'>
+          <LinkRouter className='border-t border-b py-2' to='/'>Home</LinkRouter>
+          <LinkRouter to='/'>Products</LinkRouter>
+          <LinkRouter to='/'>Contact</LinkRouter>
+        </nav>
+        <section className='flex items-center gap-4'>
+          <div className='flex gap-1'>
+            <label>
+              <input type='search' className='input-search w-[180px] text-xs bg-light-gray shadow-lg rounded-md focus:outline-none focus:bg-white p-2' placeholder='Type to search...'/>
+            </label>
+            <button><FaSearch className='text-taupe-gray text-lg' /></button>
+          </div>
+          <LinkRouter to='/' className='w-[40px] h-[40px] bg-light-green rounded-full p-2.5 hover:scale-110'><RiUserAddLine className='text-xl text-moss-green'/></LinkRouter>
+        </section>
       </section>
     </header>
   )
