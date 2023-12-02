7a6a86d12b70e3904cfc7e2e5192b9b750eb97d4
eugeniarossanigo <eugeniarossanigo@gmail.com>
2023-12-02 20:04:45 -0300

    add useLocation and scrolltotop 0,0

--- frontend/src/layouts/Layout.jsx
+++ frontend/src/layouts/Layout.jsx
@@ -1,8 +1,15 @@
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 
 export default function Layout() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+      window.scrollTo(0,0)
+  }, [pathname])
+
   return (
     <>
       <Header />
