3d34b840ec74403da8c2496a55e35f94268faf2a
eugeniarossanigo <eugeniarossanigo@gmail.com>
2023-12-01 22:52:41 -0300

    update css index and tailwind colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'montserrat': ['Montserrat', 'sans-serif'],
+        'firacode': ['Fira Code', 'monospace'], 
+      }
+    },
+    colors: {
+      'white': '#fff',
+      'black': '#000',
+      'light-gray': '#f3f3f3',
+      'light-green': '#dfe5d1',
+      'cream': '#F0F7F4',
+      'red': '#6D2020',
+      'blue': '#035E7B',
+      'moss-green': '#71816D',
+      'jasmine': '#FDE696',
+      'cerulean': '#437C90',
+      'taupe-gray': '#948B89'
+    }
   },
   plugins: [],
 }
