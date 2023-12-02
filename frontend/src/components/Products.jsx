b62f4871c269a973719163e4af8fb0a744e3d93a
eugeniarossanigo <eugeniarossanigo@gmail.com>
2023-12-02 20:03:11 -0300

    apply context and fix image data

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -1,21 +1,13 @@
 import CardProduct from './CardProduct'
-import { useState, useEffect } from 'react';
-import apiURL from '../../api';
+import { useContext } from 'react';
+import { GuitarContext } from '../context/GuitarContext';
 
 export default function Products() {
-  const [products, setProducts] = useState([])
-
-  useEffect(() => {
-    fetch(apiURL+ 'guitars')
-      .then(res => res.json())
-      .then(data => {
-        setProducts(data.response)
-      })
-  }, []);
+  const { products } = useContext(GuitarContext)
 
   return (
-    <section className='w-full sm:w-10/12 xl:w-4/5 2xl:w-3/4 flex justify-center flex-wrap pt-10 pb-16 gap-8'>
-      { products?.map(product => <CardProduct key={product.id_guitar} product={product} />) }
-    </section>
+      <section className='w-full sm:w-10/12 xl:w-4/5 2xl:w-3/4 flex justify-center flex-wrap pt-10 pb-16 gap-8'>
+        { products?.map(product => <CardProduct key={product.id_guitar} product={product} />) }
+      </section>
   )
 }
