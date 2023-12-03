b110f19c03d71fd46f9b7c8d282e30e4648f44eb
eugeniarossanigo <eugeniarossanigo@gmail.com>
2023-12-03 16:47:13 -0300

    add users router and update auth router on index

---
+++ backend/routes/authRouter.js
@@ -0,0 +1,16 @@
+import { Router } from 'express'
+const authRouter = Router()
+import userController from '../controllers/userController.js'
+import validator from '../middlewares/validator.js'
+import signupSchema from '../validators/signupValidator.js'
+import signinSchema from '../validators/signinValidator.js'
+import { generateToken, passportVerify, verifyPass, verifyUser } from '../middlewares/verifications.js'
+
+authRouter.get('/', userController.getAll)
+authRouter.get('/:id', userController.getOne)
+authRouter.post('/signup', validator(signupSchema), userController.signup)
+authRouter.post('/signin', validator(signinSchema), verifyUser, verifyPass, generateToken, userController.signin)
+authRouter.post('/token', passportVerify.authenticate('jwt', {session: false}), generateToken, userController.authenticated);
+authRouter.post('/signout', passportVerify.authenticate('jwt', {session: false}), userController.signout)
+
+export default authRouter
