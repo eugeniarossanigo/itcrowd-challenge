525f9fd63317ca3213be3b7ee163982a1e8a18d7
eugeniarossanigo <eugeniarossanigo@gmail.com>
2023-12-04 09:35:13 -0300

    add provider to main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import GuitarContextProvider from './context/GuitarContext.jsx'
+import { Provider } from 'react-redux'
+import { store } from './redux/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GuitarContextProvider>
-      <App />
-    </GuitarContextProvider>
+    <Provider store={store}>
+      <GuitarContextProvider>
+        <App />
+      </GuitarContextProvider>
+    </Provider>
   </React.StrictMode>,
 )
