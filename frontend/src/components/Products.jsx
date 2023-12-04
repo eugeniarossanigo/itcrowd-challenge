8e5490c12908444f82a28919146ecddf83f95844
eugeniarossanigo <eugeniarossanigo@gmail.com>
2023-12-04 09:45:31 -0300

    add loading or no find products

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -3,11 +3,23 @@ import { useContext } from 'react';
 import { GuitarContext } from '../context/GuitarContext';
 
 export default function Products() {
-  const { products } = useContext(GuitarContext)
+  const { products, loading } = useContext(GuitarContext)
 
   return (
       <section className='w-full sm:w-10/12 xl:w-4/5 2xl:w-3/4 flex justify-center flex-wrap pt-10 pb-16 gap-8'>
-        { products?.map(product => <CardProduct key={product.id_guitar} product={product} />) }
+        { loading ?
+          <p>Loading products...</p>
+          :
+          <>
+            { products && products.length === 0 ?
+             <p>No products match with your search...</p>
+             :
+             <>
+             { products?.map(product => <CardProduct key={product.id_guitar} product={product} />) }
+             </>
+            }
+          </>
+        }
       </section>
   )
 }
