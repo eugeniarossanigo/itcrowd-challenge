e035a7e5718ea7abce7e1e4dd9376301e0e161a6
eugeniarossanigo <eugeniarossanigo@gmail.com>
2023-12-04 10:37:31 -0300

    update apiUrl and readme

--- frontend/api.js
+++ frontend/api.js
@@ -1,3 +1,5 @@
-const apiURL = 'http://localhost:3000'
+const apiURL = import.meta.env.MODE === 'production'
+  ? import.meta.env.VITE_PROD_API_URL
+  : 'http://localhost:3000'
 
 export default apiURL
